52493f06063ba046bad72d0f3e5ef39a8446f7ba
akashgarg3a <akashgarg3a@gmail.com>
2022-06-11 22:59:06 +0530

    Adding basic files for contact App

---
+++ src/main/java/com/apress/ContactController.java
@@ -0,0 +1,18 @@
+package com.apress;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class ContactController {
+
+    private ContactRepository contactRepository;
+
+    @GetMapping("/Contacts")
+    public ResponseEntity<Iterable<Contact>>getContacts(){
+        return ResponseEntity.ok(contactRepository.findAll());
+    }
+}
